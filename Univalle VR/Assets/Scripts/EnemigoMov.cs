a2960a35aa6a32a3f7ac511a15f40e705c28ab64
Universitario <seg.wild07@gmail.com>
2018-10-22 19:20:21 -0400

    V1

---
+++ Univalle VR/Assets/Scripts/EnemigoMov.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemigoMov : MonoBehaviour {
+
+    Transform player;
+
+    NavMeshAgent nav;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //player = GameObject.Find("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nav.SetDestination(player.position);
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "bala")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+            
+        }        
+
+    }
+}
---
+++ Univalle VR/Assets/Scripts/IAControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAControl : MonoBehaviour {
+
+    UnityEngine.AI.NavMeshAgent agenteDeNavegacion;
+    public GameObject destino;
+	// Use this for initialization
+	void Start ()
+    {
+        agenteDeNavegacion = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agenteDeNavegacion.SetDestination(destino.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
